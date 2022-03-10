e7f2157215cac88c218d4b5bc82948fcf5b5e521
+ <+>
2022-03-09 21:38:04 -0500

    commit tree 5a7fa8ce0fbd4b1ac4843fa26b338fb7095c8c2d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName,email){
+    const Obj = { 
+        method:'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        "accept": 'application/json'
+    },
+    body:JSON.stringify({
+        'name': userName,
+        'email': email 
+    })
+};
+  return  fetch(`http://localhost:3000/users`, Obj)
+  .then(res => res.json())
+  .then(body => addContact(body.id))
+
+.catch(error => {
+    let message = 'Unauthorized Access'
+    const p = document.createElement('p')
+    p.innerHTML = error.message;
+    const body = document.querySelector('body');
+    body.append(p);
+})
+
+function addContact(newId){
+    document.getElementsByTagName('body')[0].innerHTML = newId;
+    document.body.append(message)
+
+submitData('name', 'name@name.com')
+
+}}
